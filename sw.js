638a4b2b6cc2b0cf319f59d0eddec8e179fc336c
Manish9823 <53974644+Manish9823@users.noreply.github.com>
2020-09-13 07:54:28 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+
+
+self.addEventListener('install', function(event){
+    console.log('Sw installed')
+    event.waitUntil(
+        caches.open('static')
+          .then(function(cache){
+            cache.addAll([
+                '/',
+                '/index.html',
+                '/img/img144.jpg',
+                '/img/img256.jpg',
+                '/img/img512.jpg',
+                '/img/img96.jpg'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', function(){
+    console.log("sw is activated")
+})
+
+self.addEventListener('fetch', function(event){
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(res){
+            if(res){
+                return res;
+            }
+            else{
+                return fetch(event.request);
+            }
+        })
+    );
+});
+
